bfd459cd74adc8130812fdef254bcdcf1fca29dd
KIMIJISU <fnfhdps@naver.com>
2024-01-02 18:09:27 +0900

    Delete FAVION_LIB directory

--- FAVION_LIB/FAVION_LIB/StdAfx.cpp
+++
@@ -1,8 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-//	FAVION_LIB.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
-
